b67881a8769b9751f7bf55b14c11eb8cd21a1f28
Sadil Manvidu <156831324+Sadil21@users.noreply.github.com>
2026-03-17 17:19:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
-import path from "path"
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss()],
+  base: '/Leo-Cub-Of-Colombo-Grand-Circle/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // <--- This maps @ to your src folder
+      '@': path.resolve(__dirname, './src'),
     },
-  base: './',
   },
 })
